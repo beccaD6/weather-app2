a64a5f8194bc10522c4a3806c95acf5f7b2c95b9
beccaD6 <becca.diaz6@gmail.com>
2020-03-01 10:01:05 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+	render(){
+		//make sure you only return everythign in one parent element
+		return (
+			<div>
+			 <Titles />
+			 <Form />
+			 <Weather />
+			</div> 
+		);
+	}
+};
 
-export default App;
+//now other folders can import this Component
+
+export default App; 
