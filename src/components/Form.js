b93a1a7d6249d99f3419a4b4c4efbbd9a86572d0
beccaD6 <becca.diaz6@gmail.com>
2020-03-01 11:57:05 +1100

    changed to stateless components

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,15 @@
 import React from "react";
 
-class Form extends React.Component{
-	
-render(){
-	return(
-		<form onSubmit={this.props.getWeather}>
+//think of Form component like an arrow fn that takes props (think of like fn params) and returns what it renders to UI
+
+const Form= props =>(
+		<form onSubmit={props.getWeather}>
 		<input type="text" name="city" placeholder="City.."/>
 		<input type="text" name="country" placeholder="country.."/>
 		<button> Get Weather </button>
 		</form>
-	);
-}
+);
 
-}
+
+
 export default Form;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,19 +1,14 @@
 import React from "react";
 
-class Weather extends React.Component{
-	
-	render(){
-		//use and operator to only display below fields if we have the input from api already/props exist
-		return(
+const Weather = (props)=>(
 			<div> 
-				{this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p> }
-				{this.props.temperature && <p>Temperature: {this.props.temperature}</p> }
-				{this.props.humidity && <p>Humidity: {this.props.humidity}</p> }
-				{this.props.description && <p>Conditions: {this.props.description}</p> }
-				{ this.props.error && <p> {this.props.error} </p> }
+				{props.city && props.country && <p>Location: {props.city}, {props.country}</p> }
+				{props.temperature && <p>Temperature: {props.temperature}</p> }
+				{props.humidity && <p>Humidity: {props.humidity}</p> }
+				{props.description && <p>Conditions: {props.description}</p> }
+				{ props.error && <p> {props.error} </p> }
 			</div>
-		);
-	}
+	);
 
-}
+
 export default Weather;
